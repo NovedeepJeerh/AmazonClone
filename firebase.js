74c9ef5cf500093b51dd135f8c45ca1c9041c899
Nove_Jeerh <68063461+NovedeepJeerh@users.noreply.github.com>
2021-05-25 15:23:36 +0530

    initial commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCL3es6olHQ7MbYMjZzMj8FEbok-ppYxIo",
+    authDomain: "clone-3387d.firebaseapp.com",
+    projectId: "clone-3387d",
+    storageBucket: "clone-3387d.appspot.com",
+    messagingSenderId: "190726697178",
+    appId: "1:190726697178:web:b4d505c7db136a87eb75e3"
+  };
