67efe8b12203f3d83ce3e70db775e768c8ace641
Nove_Jeerh <68063461+NovedeepJeerh@users.noreply.github.com>
2021-05-27 19:06:22 +0530

    initial commit

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,11 +5,32 @@ import Header from "../components/Header";
 import CheckoutProduct from "../components/CheckoutProduct";
 import Currency from "react-currency-formatter";
 import { useSession } from "next-auth/client";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 function Checkout() {
     const items = useSelector(selectItems);
     const total = useSelector(selectTotal);
     const [session] = useSession();
+
+    const createCheckoutSession = async () => {
+        const stripe = await stripePromise;
+
+        //call the backend to a create a checkout session....
+        const checkoutSession = await axios.post('/api/create-checkout-session', {
+            items:items,
+            email: session.user.email,
+        });
+
+        //Redirect user/customer to stripe Checkout session
+        const result = await stripe.redirectToCheckout({
+            sessionId: checkoutSession.data.id,
+        });
+
+        if (result.error) alert(result.error.message);
+    };
 
     return (
         <div className=" bg-gray-100">
@@ -57,8 +78,12 @@ function Checkout() {
                         </span>
                     </h2>
 
-                    <button className = {`button mt-2 ${!session && "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"}`}>
-                        {!session ? 'Sign in to checkout' : 'Proceed to checkout'}
+                    <button 
+                        role="link"
+                        onClick={createCheckoutSession} 
+                        disabled={!session} 
+                        className = {`button mt-2 ${!session && "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"}`}>
+                            {!session ? 'Sign in to checkout' : 'Proceed to checkout'}
                     </button>
                 </>
             )}
